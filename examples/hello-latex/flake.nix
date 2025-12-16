c430f0188730e54f72a92d1e1d06d6ead1bf791c
ners <ners@gmx.ch>
2025-12-16 09:23:26 +0100

    add latex example

---
+++ examples/hello-latex/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+  outputs = inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "hello";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [
+          texliveSmall
+        ];
+      };
+    };
+}
